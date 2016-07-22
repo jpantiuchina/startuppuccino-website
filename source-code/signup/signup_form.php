9123cd26eee37f5fbc96b479de3026d628b68d6c
mondial95 <mondial95@gmail.com>
2016-07-22 11:41:39 +0200

    Updated Registration Form
    The field background now is "Faculty/Company" and added a new text field
    "skills" (same of old "background" - still missing field skills in the
    database, that is at the moment data in the input skills are not stored)

--- source-code/signup/signup_form.php
+++ source-code/signup/signup_form.php
@@ -1,6 +1,6 @@
 <form action="" method="post" class="form_custom" onsubmit="return checkForm();">
 
-	<?php if($error_message != ""){ ?>
+	<?php if(isset($error_message) && $error_message != ""){ ?>
 
 		<li class="form_box_item form_box_item--full">
 			<p><?php print $error_message; ?></p>
@@ -8,10 +8,23 @@
 
 	<?php } ?>
 
+
+	<li class="form_box_item">
+		<label>Role</label>
+		<label><input type="radio" name="role" value="student" onclick="switchInputs(0)" <?php if(isset($_POST['role']) && $_POST['role']=="student") print 'checked="checked"' ?> required/>Student (I'm here to learn)</label>
+		<label><input type="radio" name="role" value="mentor"  onclick="switchInputs(1)" <?php if(isset($_POST['role']) && $_POST['role']=="mentor") print 'checked="checked"' ?> required/>Mentor (I'm here to help)</label>
+	</li>
+
+
+	<hr />
+
+
 	<li class="form_box_item">
 		<label>Email</label>
 		<input class="form_pretty_general_input" type="email" name="email" placeholder="hello@startuppucino.com" <?php if(isset($_POST['email'])) print('value="'.$_POST['email'].'"'); ?> required/>
 	</li>
+
+	<br>
 
 	<li class="form_box_item">
 		<label>Password</label>
@@ -23,6 +36,10 @@
 		<input class="form_pretty_general_input"  id="password_input_2" type="password" name="password1" required/>
 	</li>
 
+	
+	<hr />
+
+
 	<li class="form_box_item">
 		<label>Firstname</label>
 		<input class="form_pretty_general_input" type="text" name="firstname" <?php if(isset($_POST['firstname'])) print('value="'.$_POST['firstname'].'"'); ?>required/>
@@ -33,21 +50,32 @@
 		<input class="form_pretty_general_input" type="text" name="lastname" <?php if(isset($_POST['lastname'])) print('value="'.$_POST['lastname'].'"'); ?> required/>
 	</li>
 
+
 	<li class="form_box_item">
-		<label>Background</label>
-		<input class="form_pretty_general_input" type="text" name="background" placeholder="e.g. IT, design, law, economics, management" <?php if(isset($_POST['background'])) print('value="'.$_POST['background'].'"'); ?> required/>
+		<label id="background_label">Faculty(Student)/Company(Mentor)</label>
+		<input class="form_pretty_general_input" type="text" name="background" <?php if(isset($_POST['background'])) print('value="'.$_POST['background'].'"'); ?> required/>
 	</li>
+
+
+	<hr />
+
+
+	<li class="form_box_item">
+		<label>Skills (Separated by a comma - Minimum 1 skill)</label>
+		<input class="form_pretty_general_input" type="text" name="skills" placeholder="e.g. IT, design, law, economics, management" <?php if(isset($_POST['skills'])) print('value="'.$_POST['skills'].'"'); ?> required/>
+	</li>
+
+
+	<hr />
+
+	<!--
 
 	<li class="form_box_item">
 		<label>About me (optional)</label>
-		<textarea class="form_pretty_general_input" rows="3" name="about" placeholder="More info about me, about my startup idea, etc."><?php if(isset($_POST['about'])) print($_POST['about']); ?></textarea>
+		<textarea class="form_pretty_general_input" rows="3" name="about" placeholder="More info about me, about my startup idea, etc."><?php //if(isset($_POST['about'])) print($_POST['about']); ?></textarea>
 	</li>
 
-	<li class="form_box_item">
-		<label>Role</label>
-		<label><input type="radio" name="role" value="student" <?php if($_POST['role']=="student") print 'checked="checked"' ?> required/>Student (I'm here to learn)</label>
-		<label><input type="radio" name="role" value="mentor"  <?php if($_POST['role']=="mentor") print 'checked="checked"' ?> required/>Mentor (I'm here to help)</label>
-	</li>
+	-->
 
 	<?php 
 		// I suggest not to let user upload the picture here 
@@ -71,11 +99,22 @@ function checkForm(){
 	// If you want to proceed with post submitting -> return true;
 
 	// Check if password doublecheck match
-	if(document.getElementById('password_input_1').value == document.getElementById('password_input_2').value)
+	if(document.getElementById('password_input_1').value == document.getElementById('password_input_2').value){
 		return true;
-	else
+	}
+	else {
 		alert("Passwords do not match!");
 		return false;
+	}
+}
+
+function switchInputs(s){
+
+	if(s===0){ // Student
+		document.getElementById("background_label").innerHTML = "Faculty";
+	} else if(s===1){ // Mentor
+		document.getElementById("background_label").innerHTML = "Company";
+	}
 
 }
 </script>
