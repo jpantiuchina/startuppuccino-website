ed39188d3583e98b5bd33730498e26f542ec1dbf
patranka <patrankyte@gmail.com>
2016-09-08 00:07:16 +0200

    Update create_startuppuccino.sql
    forward engineered sql from new ER diagram

--- documentation/create_startuppuccino.sql
+++ documentation/create_startuppuccino.sql
@@ -1,367 +1,164 @@
--- phpMyAdmin SQL Dump
--- version 4.5.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Aug 09, 2016 at 12:03 PM
--- Server version: 5.7.11
--- PHP Version: 5.6.19
+-- MySQL Workbench Forward Engineering
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema startup
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `startup` DEFAULT CHARACTER SET utf8mb4 ;
+USE `startup` ;
+
+-- -----------------------------------------------------
+-- Table `startup`.`account`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `startup`.`account` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `linkedin_id` VARCHAR(63) NULL,
+  `about` LONGTEXT COLLATE 'utf8mb4_unicode_ci' NULL DEFAULT NULL,
+  `avatar` LONGBLOB NULL DEFAULT NULL,
+  `background` VARCHAR(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `skills` VARCHAR(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `socials` LONGTEXT COLLATE 'utf8mb4_unicode_ci' NULL DEFAULT NULL,
+  `email` VARCHAR(63) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `firstName` VARCHAR(63) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `lastName` VARCHAR(63) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `password` VARCHAR(63) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `role` VARCHAR(15) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `account_email` (`email` ASC),
+  UNIQUE INDEX `linkedin_id_UNIQUE` (`linkedin_id` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+COLLATE = utf8mb4_unicode_ci;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- -----------------------------------------------------
+-- Table `startup`.`project`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `startup`.`project` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(63) COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  `owner_id` INT(11) NOT NULL,
+  `description` LONGTEXT COLLATE 'utf8mb4_unicode_ci' NULL DEFAULT NULL,
+  `vision` LONGTEXT COLLATE 'utf8mb4_unicode_ci' NULL DEFAULT NULL,
+  `avatar` TEXT NULL,
+  `ideal_team_size` INT(2) NULL,
+  `current_team_size` INT(2) NULL,
+  `looking_for` VARCHAR(63) NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `approved` CHAR(1) NOT NULL DEFAULT 'F',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `title` (`title` ASC),
+  INDEX `fk_project_author_idx` (`owner_id` ASC),
+  CONSTRAINT `fk_project_author`
+    FOREIGN KEY (`owner_id`)
+    REFERENCES `startup`.`account` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+COLLATE = utf8mb4_unicode_ci;
 
---
--- Database: `startuppuccino`
---
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `startup`.`comment`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `startup`.`comment` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `project_id` INT(11) NOT NULL,
+  `author_id` INT(11) NOT NULL,
+  `text` LONGTEXT COLLATE 'utf8mb4_unicode_ci' NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `comment_project` (`project_id` ASC),
+  INDEX `comment_author` (`author_id` ASC),
+  CONSTRAINT `comment_author`
+    FOREIGN KEY (`author_id`)
+    REFERENCES `startup`.`account` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `comment_project`
+    FOREIGN KEY (`project_id`)
+    REFERENCES `startup`.`project` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+COLLATE = utf8mb4_unicode_ci;
 
---
--- Table structure for table `account`
---
 
-CREATE TABLE `account` (
-  `id` int(11) NOT NULL,
-  `about` longtext COLLATE utf8mb4_unicode_ci,
-  `avatar` longblob,
-  `background` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `skills` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `socials` longtext COLLATE utf8mb4_unicode_ci,
-  `email` varchar(63) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `firstName` varchar(63) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lastName` varchar(63) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(63) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- -----------------------------------------------------
+-- Table `startup`.`project_participants`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `startup`.`project_participants` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `project_id` INT(11) NOT NULL,
+  `account_id` INT(11) NOT NULL,
+  `joined_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idea_id` (`project_id` ASC, `account_id` ASC),
+  INDEX `fk_account_id_idx` (`account_id` ASC))
+ENGINE = MyISAM
+AUTO_INCREMENT = 58
+DEFAULT CHARACTER SET = latin1;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `comment`
---
+-- -----------------------------------------------------
+-- Table `startup`.`like`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `startup`.`like` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `project_id` INT(11) NOT NULL,
+  `account_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `like_project` (`project_id` ASC),
+  INDEX `like_account` (`account_id` ASC),
+  UNIQUE INDEX `unique_likes` (`project_id` ASC, `account_id` ASC),
+  CONSTRAINT `like_account`
+    FOREIGN KEY (`account_id`)
+    REFERENCES `startup`.`account` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `like_project`
+    FOREIGN KEY (`project_id`)
+    REFERENCES `startup`.`project` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+COLLATE = utf8mb4_unicode_ci;
 
-CREATE TABLE `comment` (
-  `id` int(11) NOT NULL,
-  `project_id` int(11) DEFAULT NULL,
-  `account_id` int(11) DEFAULT NULL,
-  `author_id` int(11) DEFAULT NULL,
-  `text` longtext COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `startup`.`milestone`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `startup`.`milestone` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(63) NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
+  `deadline` DATETIME NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name` (`name` ASC))
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
 
---
--- Table structure for table `ideaaccount`
---
 
-CREATE TABLE `ideaaccount` (
-  `id` int(11) NOT NULL,
-  `idea_id` int(11) NOT NULL,
-  `account_id` int(11) NOT NULL,
-  `date` date DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+-- -----------------------------------------------------
+-- Table `startup`.`project_milestones`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `startup`.`project_milestones` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `project_id` INT(11) NOT NULL,
+  `milestone_id` INT(11) NOT NULL,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `project_milestones_idx` (`project_id` ASC),
+  INDEX `fk_milestone_p_idx` (`milestone_id` ASC))
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `ideacomment`
---
-
-CREATE TABLE `ideacomment` (
-  `id` int(11) NOT NULL,
-  `account_id` int(11) NOT NULL,
-  `idea_id` int(11) NOT NULL,
-  `text` text NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `idealike`
---
-
-CREATE TABLE `idealike` (
-  `id` int(11) NOT NULL,
-  `account_id` int(11) NOT NULL,
-  `idea_id` int(11) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ideas`
---
-
-CREATE TABLE `ideas` (
-  `id` int(11) NOT NULL,
-  `title` varchar(63) NOT NULL,
-  `description` varchar(144) DEFAULT NULL,
-  `owner_id` int(11) NOT NULL,
-  `team_size` int(2) NOT NULL DEFAULT '2',
-  `avatar` text,
-  `background_pref` longtext,
-  `approved` char(1) NOT NULL DEFAULT 'F',
-  `date` date DEFAULT NULL,
-  `current_team_size` int(2) NOT NULL DEFAULT '1'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `like`
---
-
-CREATE TABLE `like` (
-  `id` int(11) NOT NULL,
-  `project_id` int(11) DEFAULT NULL,
-  `account_id` int(11) DEFAULT NULL,
-  `author_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `milestones`
---
-
-CREATE TABLE `milestones` (
-  `id` int(11) NOT NULL,
-  `name` varchar(63) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `project`
---
-
-CREATE TABLE `project` (
-  `id` int(11) NOT NULL,
-  `description` longtext COLLATE utf8mb4_unicode_ci,
-  `title` varchar(63) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `vision` longtext COLLATE utf8mb4_unicode_ci,
-  `team_id` int(11) NOT NULL,
-  `created_date` date NOT NULL,
-  `updated_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `projectmilestones`
---
-
-CREATE TABLE `projectmilestones` (
-  `project_id` int(11) NOT NULL,
-  `milestone_id` int(11) NOT NULL,
-  `update_date` date NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teamaccount`
---
-
-CREATE TABLE `teamaccount` (
-  `id` int(11) NOT NULL,
-  `team_id` int(11) NOT NULL,
-  `account_id` int(11) NOT NULL,
-  `date` date DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teams`
---
-
-CREATE TABLE `teams` (
-  `id` int(11) NOT NULL,
-  `name` varchar(63) NOT NULL,
-  `mentor_id` int(11) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `account_email` (`email`);
-
---
--- Indexes for table `comment`
---
-ALTER TABLE `comment`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `comment_project` (`project_id`),
-  ADD KEY `comment_account` (`account_id`),
-  ADD KEY `comment_author` (`author_id`);
-
---
--- Indexes for table `ideaaccount`
---
-ALTER TABLE `ideaaccount`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `idea_id` (`idea_id`,`account_id`);
-
---
--- Indexes for table `ideacomment`
---
-ALTER TABLE `ideacomment`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `idealike`
---
-ALTER TABLE `idealike`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `account_id` (`account_id`,`idea_id`);
-
---
--- Indexes for table `ideas`
---
-ALTER TABLE `ideas`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `title` (`title`);
-
---
--- Indexes for table `like`
---
-ALTER TABLE `like`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `like_unique_project_author` (`project_id`,`author_id`),
-  ADD UNIQUE KEY `like_unique_account_author` (`account_id`,`author_id`),
-  ADD KEY `like_project` (`project_id`),
-  ADD KEY `like_account` (`account_id`),
-  ADD KEY `like_author` (`author_id`);
-
---
--- Indexes for table `milestones`
---
-ALTER TABLE `milestones`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
-
---
--- Indexes for table `project`
---
-ALTER TABLE `project`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `title` (`title`),
-  ADD UNIQUE KEY `team_id` (`team_id`);
-
---
--- Indexes for table `projectmilestones`
---
-ALTER TABLE `projectmilestones`
-  ADD PRIMARY KEY (`project_id`,`milestone_id`);
-
---
--- Indexes for table `teamaccount`
---
-ALTER TABLE `teamaccount`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `teams`
---
-ALTER TABLE `teams`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `account`
---
-ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `comment`
---
-ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `ideaaccount`
---
-ALTER TABLE `ideaaccount`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
---
--- AUTO_INCREMENT for table `ideacomment`
---
-ALTER TABLE `ideacomment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `idealike`
---
-ALTER TABLE `idealike`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
---
--- AUTO_INCREMENT for table `ideas`
---
-ALTER TABLE `ideas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
---
--- AUTO_INCREMENT for table `like`
---
-ALTER TABLE `like`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `milestones`
---
-ALTER TABLE `milestones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `project`
---
-ALTER TABLE `project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `teamaccount`
---
-ALTER TABLE `teamaccount`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `teams`
---
-ALTER TABLE `teams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `comment`
---
-ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_account` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `comment_author` FOREIGN KEY (`author_id`) REFERENCES `account` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `comment_project` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `like`
---
-ALTER TABLE `like`
-  ADD CONSTRAINT `like_account` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `like_author` FOREIGN KEY (`author_id`) REFERENCES `account` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `like_project` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
