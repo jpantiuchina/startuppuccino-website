5f5cd52164c3d42c9694060fbb5dcfd9c226c73a
jenny <patrankyte@gmail.com>
2016-10-05 17:55:48 +0200

    comment deletion fixed.  php error notifications disabled

--- source-code/app/controllers/course_sessions_comments.php
+++ source-code/app/controllers/course_sessions_comments.php
@@ -1,38 +1,38 @@
 <?php
+error_reporting(0);
+// Receive http request from client with a new comment to post
 
-	// Receive http request from client with a new comment to post
+// Start session
+require_once '../models/session.php';
 
-	// Start session
-	require_once '../models/session.php';
+// Check if the user has the rights to perform this operation
+//if(!$_SESSION['role'] === "mentor"){
+//	exit("You are not allowed to make these changes.");
+//}
 
-	// Check if the user has the rights to perform this operation
-	//if(!$_SESSION['role'] === "mentor"){
-	//	exit("You are not allowed to make these changes.");
-	//}
+// Check if the parameter is set
+if( !isset($_POST['s_id']) || empty($_POST['s_id']) ||
+    (isset($_POST['delete']) && (!isset($_POST['comment_id']) || empty($_POST['comment_id'])) ) ||
+    (isset($_POST['new']) && (!isset($_POST['comment_text']) || empty($_POST['comment_text'])) ) ){
+    exit("Some parameter is missing.");
+}
 
-	// Check if the parameter is set
-	if( !isset($_POST['s_id']) || empty($_POST['s_id']) ||
-		(isset($_POST['delete']) && (!isset($_POST['comment_id']) || empty($_POST['comment_id'])) ) ||
-	    (isset($_POST['new']) && (!isset($_POST['comment_text']) || empty($_POST['comment_text'])) ) ){
-		exit("Some parameter is missing.");
-	}
+// Include Course sessions functions
+require_once '../models/CourseSessions_Functions.php';
+$cs_func = new CourseSessions_Functions();
 
-	// Include Course sessions functions
-	require_once '../models/CourseSessions_Functions.php';
-	$cs_func = new CourseSessions_Functions();	
+if(isset($_POST['delete'])){
+    // Delete comment
+    $response = $cs_func->comment($_SESSION['id'], $_POST['s_id'], null, TRUE, $_POST['comment_id']);
+} else if (isset($_POST['new'])){
+    // Save comment
+    $response = $cs_func->comment($_SESSION['id'], $_POST['s_id'], $_POST['comment_text']);
+}
 
-	if(isset($_POST['delete'])){
-		// Delete comment
-		$response = $cs_func->comment($_SESSION['id'], $_POST['s_id'], null, TRUE, $_POST['comment_id']);
-	} else {
-		// Save comment
-		$response = $cs_func->comment($_SESSION['id'], $_POST['s_id'], $_POST['comment_text']);	
-	}
-	
-	if ( $response ){
-		echo "ok";
-	} else {
-		echo "Error while saving data, please try again.";
-	}
+if ( $response ){
+    echo "ok";
+} else {
+    echo "Error while saving data, please try again.";
+}
 
 ?>
