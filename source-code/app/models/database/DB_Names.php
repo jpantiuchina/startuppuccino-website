6aa317d7d849e3c33b315244191742c9d98d2b6f
mondial95 <mondial95@gmail.com>
2016-10-14 10:50:33 +0200

    Added prefix to tables

--- source-code/app/models/database/DB_Names.php
+++ source-code/app/models/database/DB_Names.php
@@ -3,22 +3,25 @@
 /**
  * Database table name variables
  */
-define("_T_ACCOUNT", "account"); //+
-define("_T_IDEA", "project"); //+
-define("_T_IDEA_ACCOUNT", "project_participant");
-define("_T_IDEA_COMMENT", "project_comment");
-define("_T_IDEA_LIKE", "project_like");
-define("_T_LIKE", "like");
-define("_T_COMMENT","comment");
+
+define("DB_TABLE_PREFIX", "startuppuccino__");
+
+define("_T_ACCOUNT", DB_TABLE_PREFIX."account"); //+
+define("_T_IDEA", DB_TABLE_PREFIX."project"); //+
+define("_T_IDEA_ACCOUNT", DB_TABLE_PREFIX."project_participant");
+define("_T_IDEA_COMMENT", DB_TABLE_PREFIX."project_comment");
+define("_T_IDEA_LIKE", DB_TABLE_PREFIX."project_like");
+define("_T_LIKE", DB_TABLE_PREFIX."like");
+define("_T_COMMENT", DB_TABLE_PREFIX."comment");
 //define("_T_TEAM", "teams");
 //define("_T_TEAM_ACCOUNT", "teamaccount");
-define("_T_PROJECT", "project");
-define("_T_MILESTONE", "milestone");
-define("_T_PROJECT_MILESTONE", "project_milestones");
+define("_T_PROJECT", DB_TABLE_PREFIX."project");
+define("_T_MILESTONE", DB_TABLE_PREFIX."milestone");
+define("_T_PROJECT_MILESTONE", DB_TABLE_PREFIX."project_milestones");
 
-define("_T_SESSION", "session");
-define("_T_SESSION_COMMENT", "session_comment");
-define("_T_MENTOR_AVAILABILITY", "mentor_availability");
-define("_T_ACCOUNT_LOGGED", "account_logged");
+define("_T_SESSION", DB_TABLE_PREFIX."session");
+define("_T_SESSION_COMMENT", DB_TABLE_PREFIX."session_comment");
+define("_T_MENTOR_AVAILABILITY", DB_TABLE_PREFIX."mentor_availability");
+define("_T_ACCOUNT_LOGGED", DB_TABLE_PREFIX."account_logged");
 
 ?>
